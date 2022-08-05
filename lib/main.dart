b2ae2043c0ea37ffa0c6ddf09cae11423101eeb0
Melisa Ates <101855839+Melisaates@users.noreply.github.com>
2022-08-05 17:45:40 +0300

    title: melisa to alper

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Melisa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Melisa",
+      title: "Alper",
       theme: ThemeData(primarySwatch: Colors.red),
       home: const MyHomePage(title: "salla"),
       //padding:EdgeInsets.all(20)
